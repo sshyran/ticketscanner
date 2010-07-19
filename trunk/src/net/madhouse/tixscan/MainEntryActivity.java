e9f23c79e668a1c8ceefb10648776fe8a349f2f7
memnus@gmail.com <memnus@gmail.com@0fa215b1-a7f4-057a-21e9-cac0b6f82b2b>
2010-07-19 04:52:40 +0000

    Initial commit. Manifest file, template Java and layout XML files for all activities.

---
+++ trunk/src/net/madhouse/tixscan/MainEntryActivity.java
@@ -0,0 +1,13 @@
+package net.madhouse.tixscan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainEntryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_entry);
+    }
+}
---
+++ trunk/src/net/madhouse/tixscan/ScanCentralActivity.java
@@ -0,0 +1,14 @@
+package net.madhouse.tixscan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScanCentralActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_central);
+    }
+
+}
